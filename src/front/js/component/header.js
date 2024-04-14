459e187760c28e6e703d5b7b654a3bcf68512360
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-14 04:21:00 +0000

    dashboard made

---
+++ src/front/js/component/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify} from "react-icons/bs"
+
+export function Header({openSidebarToggle}) {
+  return (
+    <div className="header">
+        <div className="menu-icon">
+            <BsJustify className='icon' onClick={openSidebarToggle}/>
+        </div>
+        <div className="header-left">
+            <BsSearch className="icon"/>
+        </div>
+        <div className="header-right">
+            <BsFillBellFill className="icon"/>
+            <BsFillEnvelopeFill className="icon"/>
+            <BsPersonCircle className="icon"/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
